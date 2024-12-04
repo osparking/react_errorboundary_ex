a1d11b7686d335f49b6739f0062d0a5a23782751
osparking <jbpark03@gmail.com>
2024-12-04 17:45:19 +0900

    compo that contains non existing compo

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 "use client";
 import { ErrorBoundary } from "react-error-boundary";
+import ContainsNotExistingComponent from "./components/ContainsNotExistingComponent";
 export default function App() {
   return (
     <>
       <div>
         <ErrorBoundary fallback={<div>뭔가 잘못되었다.</div>}>
-          <NotExistingComponent />
+          <ContainsNotExistingComponent />
         </ErrorBoundary>
       </div>
     </>
