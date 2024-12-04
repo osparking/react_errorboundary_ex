152eaf6ec07c5eeb738f08360a8e7d35f43c4501
osparking <jbpark03@gmail.com>
2024-12-04 19:17:02 +0900

    fallbackComponent prop allows error message show

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,12 @@
 "use client";
 import { ErrorBoundary } from "react-error-boundary";
 import ContainsNotExistingComponent from "./components/ContainsNotExistingComponent";
+import Fallback from "./components/Fallback";
 export default function App() {
-  function fallbackRender({ error, resetErrorBoundary }) {
-    // resetErrorBoundary() 를 호출하여 오류 경계를 리셋하고 랜더링을 재 시도한다.
-    return (
-      <div role="alert">
-        <p>오류가 발생되었다.</p>
-        <pre style={{ color: "red" }}>{error.message}</pre>
-      </div>
-    );
-  }
   return (
     <>
       <div>
-        <ErrorBoundary fallbackRender={fallbackRender}>
+        <ErrorBoundary FallbackComponent={Fallback}>
           <ContainsNotExistingComponent />
         </ErrorBoundary>
       </div>
