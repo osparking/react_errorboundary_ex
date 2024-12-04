fab8022db7cd7fe8c3cbd6bef19ca5b854f302ba
osparking <jbpark03@gmail.com>
2024-12-04 15:25:40 +0900

    ErrorBoundary lib' compo' is used

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
+"use client";
+import { ErrorBoundary } from "react-error-boundary";
 export default function App() {
   return (
     <>
       <div>
-        <NotExistingComponent />
+        <ErrorBoundary fallback={<div>뭔가 잘못되었다.</div>}>
+          <NotExistingComponent />
+        </ErrorBoundary>
       </div>
     </>
   );
